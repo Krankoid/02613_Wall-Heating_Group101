5c8b1df75694481ff80916506c14d7b5e4bc3bf2
Thomas Riis <s214744@dtu.dk>
2026-04-11 21:29:40 +0200

    Updates to task 5

---
+++ tasks/task5/parallelize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -J parallelize
+#BSUB -q hpc
+#BSUB -n 16
+#BSUB -W 24:00
+#BSUB -R "span[hosts=1]"
+#BSUB -R "select[model==XeonGold6226R]"
+#BSUB -R "rusage[mem=300MB]" # Roughly 300MB used 
+#BSUB -oo tasks/task5/parallel_times_%J.out
+#BSUB -eo tasks/task5/parallel_times_%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+export PYTHONPATH="$PWD:$PYTHONPATH"
+python -u parallelize.py 16 50
