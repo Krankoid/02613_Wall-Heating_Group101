2083d2bd352a901e567fb95c505a5c505106350a
thalebsund <thalebsund@icloud.com>
2026-04-14 12:54:55 +0200

    changes to job

--- tasks/task8/run_CUDA.sh
+++ tasks/task8/run_CUDA.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #BSUB -J custom_cuda
-#BSUB -q c02613
+#BSUB -q gpua100
 #BSUB -n 4
+#BSUB -R "span[hosts=1]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 30
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[gpu80gb]"
+#BSUB -W 00:30
 #BSUB -o output_%J.out
 #BSUB -e error_%J.err
+
+
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
